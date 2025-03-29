4ec701d10bc210d013fb8a28e31a45e408c78299
Manimaran <manimaran.gopinathan@gmail.com>
2025-03-29 21:31:40 +0530

    American fellow commit

---
+++ src/test/java/git/AmaricanArchitech.java
@@ -0,0 +1,11 @@
+package git;
+
+public class AmaricanArchitech {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("i will create Extend Report");
+System.out.println("i will create prop files and Base Class");
+	}
+
+}
