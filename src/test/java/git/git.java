b15ac82fa87d4b60f8deef7e72a9e1c2c34650ac
Manimaran <manimaran.gopinathan@gmail.com>
2025-03-29 19:03:57 +0530

    Indian time zone guy

---
+++ src/test/java/git/git.java
@@ -0,0 +1,11 @@
+package git;
+
+public class git {
+	
+   public static void main(String[] args) {
+	System.out.println("This is manimaran");
+	System.out.println("This is from indian time zone");
+	System.out.println("I am doing great here");
+}
+
+}
