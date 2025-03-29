3cae945c2d5bd671114f6c80b7f206a6abfb931e
Manimaran <manimaran.gopinathan@gmail.com>
2025-03-29 19:23:06 +0530

    Heaven guy commit

--- src/test/java/git/git.java
+++ src/test/java/git/git.java
@@ -6,6 +6,9 @@ public class git {
 	System.out.println("This is manimaran");
 	System.out.println("This is from indian time zone");
 	System.out.println("I am doing great here");
+	System.out.println("This is leo");
+	System.out.println("Leo is from heaven");
+	System.out.println("He is completely alright");
 }
 
 }
