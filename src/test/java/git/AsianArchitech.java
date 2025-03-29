425ad59a5865f6e61a1a7d5208092827c9a62638
Manimaran <manimaran.gopinathan@gmail.com>
2025-03-29 20:39:55 +0530

    Asian Architech Commit

---
+++ src/test/java/git/AsianArchitech.java
@@ -0,0 +1,11 @@
+package git;
+
+public class AsianArchitech {
+
+	public static void main(String[] args) {
+		System.out.println("i will page object model for the project");
+		System.out.println("i create factory design pattern for the project");
+
+	}
+
+}
